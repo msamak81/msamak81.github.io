3dfabe8e86bfa963ca6da9e2bfe9466e8aac3206
msamak <samak_graphics@hotmail.com>
2018-11-27 10:58:51 +0300

    my stc updates

--- MySTC/js/functions.js
+++ MySTC/js/functions.js
@@ -7,6 +7,9 @@ $(document).ready(function(){
     $("header").load("../../templates/templates.html #header");
     $("footer").load("../../templates/templates.html #footer");
     $("#panel").load("../../templates/templates.html #widgetspanel");
+    $(window).on('load',function(){
+        if ( $('.dounghtcanvas').length ) generateDounght();
+    })
 
     $(window).on('load',function(){
         if ( $(this).width() < 993 ) {
@@ -29,15 +32,13 @@ $(document).ready(function(){
             $('.languages').detach().insertBefore('.profile');
         }
     });
-    // WOW animation init
-    //new WOW().init();
 
-    var barchartcolors =['#977aa8','#712b81', '#da3f7b', '#fadf46', '#fab53c', '#977aa8'];
-    var topdialedvalues_2 =  [66, 30, 25, 95, 80, 60 ];
-    var topdialednumbers =  ['0534343963', '0112345678', '0512387653', '0531234567', '0567658904', '0123456789' ];
+
+    var chartcolors =['#977aa8','#712b81', '#da3f7b', '#fadf46', '#fab53c', '#977aa8'];
     var packages = ['#712c81', 'rgba(242, 242, 242, 0.1)'];
     var freebies = ['#d73474', 'rgba(242, 242, 242, 0.1)'];
     var localinternet =  [40, 60 ];
+    var piecharts_val =  [63.08, 32.19, 3.68, 0.92, 0.12 ];
     var freebiesData =  [70, 30 ];
     var topdialedvalues =[
                         {
@@ -94,6 +95,22 @@ $(document).ready(function(){
         }
     ];
 
+    var pieDate =[{
+        data: [
+            63.08, 32.19, 3.68, 0.92, 0.12
+        ],
+        backgroundColor: [
+            '#977aa8','#712b81', '#da3f7b', '#fadf46', '#fab53c'
+        ],
+    }];
+    var pie_labels= [
+        'مكالمة داخل شبكة الجوال',
+        'صوتية',
+        'رسالة صوتية',
+        'هاتف ثابت',
+        'الهاتف الثابت (شبكة اخري)'
+    ];
+
     // change the z-index of bootstrap select parent
     $('.selectpicker').each(function(){
         $(this).parents('.widgetcontainer').css('z-index','1')
@@ -112,7 +129,17 @@ $(document).ready(function(){
                 id = $this.attr('id'),
                 desktop = $this.data('items-desktop'),
                 tablet = $this.data('items-tablet'),
-                mobile = $this.data('items-mobile');
+                mobile = $this.data('items-mobile'),
+                nav = $this.data('nav');
+
+               if(nav== undefined){
+                    nav = false;
+                }
+                else{
+                    nav = true;
+               }
+
+               console.log(nav)
 
             var carousel = $("#"+id).owlCarousel({
                 loop:false,
@@ -121,15 +148,17 @@ $(document).ready(function(){
                 responsive:{
                     0:{
                         items:eval(mobile),
-                        nav:false
+                        nav:nav,
+                        loop:false
                     },
                     600:{
                         items:eval(tablet),
-                        nav:false
+                        nav:nav,
+                        loop:false
                     },
                     1000:{
                         items:eval(desktop),
-                        nav:false,
+                        nav:nav,
                         loop:false
                     }
                 }
@@ -182,9 +211,66 @@ $(document).ready(function(){
         })
     };
 
+    // Pie charts configuration
+    function generatePie(){
+        var pieItem = $('.piecharts');
+        pieItem.each(function(index,ele){
+            var $this = $(ele),
+                id = $this.attr('id'),
+                src = $this.data('source'),
+                labels = $this.data('labels');
+            var ctx =$('#'+id);
+            var myPie = new Chart(ctx, {
+                type: 'pie',
+                data: {
+                    datasets:eval(src),
+                    labels: eval(labels)
+                },
+                options:{
+                    responsive: true,
+                    maintainAspectRatio:true,
+                    aspectRatio:6,
+                    legend: {
+                        display: false,
+                        position: "right",
+                        fullWidth: false,
+                        labels: {
+                            boxWidth:13,
+                            fontColor: '#712c81',
+                            fontFamily :'Open Sans'
+                        },
+                    },
+                    title: {
+                        display: true,
+                    },
+                    tooltips:{
+                        enabled:true,
+                    },
+                    animation: {
+                        animateScale: true,
+                        animateRotate: true
+                    },
+                    layout: {
+                        padding: {
+                            left: 20,
+                            right: 20,
+                            top: 0,
+                            bottom: 0
+                        }
+                    }
+
+                },
+            });
+
+            $this.parents('.piechartsholder').append('<div class="chart_legend"/>')
+            $(pieItem).parents('.piechartsholder').find('.chart_legend').html( myPie.generateLegend())
+        })
+    };
+
 //bar charts configuration
     function generateBar() {
         var barchartItem = $('.barcharts');
+
         barchartItem.each(function (index, ele) {
             var $this = $(ele),
                 id = $this.attr('id'),
@@ -193,24 +279,17 @@ $(document).ready(function(){
                 steps = $this.data('steps');
 
             var ctx = $('#' + id);
+
             var myBarChart = new Chart(ctx, {
                 type: 'bar',
                 data: {
                     datasets:eval(src),
-                    //  datasets: [{
-                    //      label:eval(labels),
-                    //      data: eval(src),
-                    //      fill: '#ffffff',
-                    //      backgroundColor: barchartcolors,
-                    //      borderColor: barchartcolors,
-                    //      borderWidth: 1,
-                    //
-                    //  }]
                 },
                 options: {
                     responsive: true,
+                    maintainAspectRatio: false,
                     legend: {
-                        display: true,
+                        display: false,
                         position: "bottom",
                         fullWidth: true,
                         labels: {
@@ -260,8 +339,8 @@ $(document).ready(function(){
                 },
             });
 
-
-            $('.chart_legend').html( myBarChart.generateLegend())
+            $this.parents('.barcharts_holder').append('<div class="chart_legend"/>')
+            $(barchartItem).parents('.barcharts_holder').find('.chart_legend').html( myBarChart.generateLegend())
 
         })
     }
@@ -276,7 +355,7 @@ $(document).ready(function(){
             $this.on('click',function(){
 
                 if (toHide == undefined){
-                    $('.hiddenContent').hide()
+                    item.parents().find('.hiddenContent').hide();
                 }
                 else{
                     $(toHide).hide();
@@ -312,13 +391,144 @@ $(document).ready(function(){
 
     }
 
+// accordion function
+    function accordions(){
+        var accordion= $('.accordion_parent');
+        accordion.each(function (index, ele) {
+            var $this = $(ele);
+            $this.attr('id', 'accordion_parent_'+index);
+            var id = $this.attr('id');
+
+            $('#'+id).find('.accordion_item').each(function(i, e){
+                var $this = $(e);
+                $this.find('.accordion_body').attr('id', id+ '_accordion_body_' + i);
+                $this.find('.accordion_header[data-toggle="collapse"]').attr('data-target', '#'+id+ '_accordion_body_' + i )
+            })
+        })
+    }
+
+    // datatables common function
+
+    function generateTables(){
+        var tables = $('.data_tables');
+        tables.each(function(i,e){
+            var $this = $(this),
+                grouping = $this.data('row-grouping'),
+                groupColumn=  $this.data('grouping-column'),
+                records_label  = $this.data('records-label'),
+                groupOrder = $this.data('grouping-order'),
+                resultsCount= $this.data('results-count'),
+                lengthArray = [10, 25, 50, 75, 100];
+
+            if(resultsCount == undefined){
+                resultsCount = 10;
+            }
+            else{
+
+                resultsCount = resultsCount;
+                lengthArray.push(resultsCount);
+                lengthArray.sort((a, b) => a - b);
+            }
+
+            var table = $this.DataTable({
+                "lengthChange": false,
+                "lengthMenu":lengthArray,
+                "displayLength": eval(resultsCount),
+                'responsive': {
+                    details: {
+                        type: 'column'
+                    }
+                },
+                "language": {
+                    "lengthMenu": "Display _MENU_ "+ records_label,
+                    "zeroRecords": "Nothing found - sorry",
+                    "info": "Showing page _PAGE_ of _PAGES_",
+                    "infoEmpty": "No records available",
+                    "infoFiltered": "(filtered from _MAX_ total records)",
+                    "search":  "Filter : ",
+                },
+                rowGroup: {
+                    enable: eval(grouping),
+                    dataSrc: groupColumn
+
+                }
+            } );
+
+            if (grouping == true){
+                table
+                    .order( [[ groupColumn, groupOrder ]])
+                    .columns( [groupColumn] ).visible( false)
+                    .draw();
+            }
+        })
+
+        // filter table actions
+
+        $('.filter_table .action_btn').click(function(){
+            var $parent= $(this).parents('.filter_table');
+
+            if($parent.hasClass('open')){
+                $parent.removeClass('open');
+            }
+            else{
+                $('.filter_table').removeClass('open');
+                $parent.addClass('open');
+            }
+
+            // check all and uncheck all checkboxes when selecting
+            var $select_all_checkbox =  $('.open .filter-table-options input:checkbox[value="all"]'),
+                $other_checkboxes = $('.open .filter-table-options input:checkbox');
+
+            $select_all_checkbox.change(function(){
+                $other_checkboxes.prop('checked', $(this).prop("checked"));
+            });
+
+            $other_checkboxes.not('input:checkbox[value="all"]').change(function(){
+                if(false == $(this).prop("checked")){
+                    $select_all_checkbox.prop('checked', false);
+                }
+                if ( $('.open .filter-table-options input:checkbox:checked').not('input:checkbox[value="all"]').length == $other_checkboxes.not('input:checkbox[value="all"]').length ){
+                    $select_all_checkbox.prop('checked', true);
+                }
+            });
+            return false;
+        })
+
+        $(document).click(function(){
+            $('.filter_table').removeClass('open');
+        })
+
+        $('.filter-table-options').click(function (e) {
+            e.stopPropagation();
+
+         })
+
+
+        // filter table based on filteration options
+
+        $('.filter-table-options input:checkbox').click(function(){
+                console.log($(this).val());
+        });
+
+
+
+
+    }
 
     // call the functions only if the item is placed in the page
     if ( $('.owl-carousel').length ) generateCarousel();
     if ( $('.dounghtcanvas').length ) generateDounght();
+    if ( $('.piecharts').length ) generatePie();
     if ( $('.barcharts').length ) generateBar();
     if ( $('.showContent').length ) showContent();
     if ( $('.add_card').length )  addNewCC();
+    if ( $('.accordion_parent').length ) accordions();
+    if ( $('.data_tables').length ) generateTables();
+
+
+
+
+
 
 });
 // END of document.ready() function
@@ -356,14 +566,22 @@ $(window).on('load', function(){
         $('body').toggleClass('open_widgets');
     });
 
-    $(document).on('click', function(){
+    $('.mainsection, footer, header').on('click', function(){
         $('body').removeClass('open_widgets');
         $('.profile').removeClass('open');
     });
-    $('#panel, .widgets_btn, .profile, .dropdown_list').on('click', function(event){
-        event.stopPropagation();
+    $('.widgets_btn, .profile, .account .dropdown_list').on('click', function(e){
+            e.stopPropagation();
     });
+    // $('#panel').on('click', function(event){
+    //     if(!$(event.target).is('.addwidget')){
+    //         event.stopPropagation()
+    //     }
+    // });
+
+
+
+    // $('#panel .form-input').on('click', function(event){
+    //     event.P
+    // });
 });
-
-
-
